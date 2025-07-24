d26c7d02c8ef1156706012e8d671d9811013c949
Charlotte <cnkuna920@gmail.com>
2025-07-24 12:17:51 +0200

    added new files

--- arrow.js
+++ arrow.js
@@ -21,4 +21,16 @@ const a = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
     const a3 = a.map((s) => s.length);
 console.log("Using Arrow Function ", a3);
 
+let num1 = 34;
+let num2 = 11;
+
+let subtraction = (num1, num2) =>{
+  num1 = num1;
+  num2 = num2
+  let answer = num1 - num2;
+  console.log(answer);
+}
+subtraction(num1 , num2)
+//you can use var, const, let to declare
+
   
---
+++ modulas.js
@@ -0,0 +1,10 @@
+//anonymous function
+//it only has a variable
+let num1, num2;
+let modulas = function(num1, num2){
+    num1 = 100;
+    num2 = 5;
+    let answer = num2 % num1;
+    console.log(answer)
+}
+modulas(num1, num2);
